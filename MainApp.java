70e6e2a2ea47e2026315fdc1ec6958de8ee03ce0
Eric Chauvin <162734842+EricsSourceCode@users.noreply.github.com>
2024-05-20 09:44:38 -0600

    Add files via upload

--- MainApp.java
+++ MainApp.java
@@ -29,7 +29,7 @@ import java.io.IOException;
 class MainApp implements Runnable, Serializable
   {
   public static final String versionDate =
-                              "4/29/2024";
+                              "5/20/2024";
   public static final long serialVersionUID = 1;
 
 
